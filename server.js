998b8dd31483c7a9ab693fc90510fffd7bc8219a
John Rayes <live627@gmail.com>
2021-09-22 20:34:54 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var formidable = require('formidable');
+
+http.createServer(function (req, res)
+{
+	var form = new formidable.IncomingForm();
+	form.parse(req, (err, fields, files) =>
+	{
+		res.writeHead(200, { 'content-type': 'application/json' });
+		res.end(JSON.stringify({ fields, files }, null, 2));
+	});
+}).listen(8086);
